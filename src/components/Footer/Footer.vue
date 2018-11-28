e6b967dba04cd03eef4ba9f82c7ecbf90089dcd2
lsJoker <576062243@qq.com>
2018-11-28 11:08:16 +0800

    1

---
+++ src/components/Footer/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="footer" class="align-center space-between"></div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  methods: {}
+};
+</script>
+
+<style lang="scss">
+$activeColor: rgb(0, 255, 213);
+#footer {
+  flex: 0 0 50px;
+  padding: 0 10px;
+  background-color: #eee;
+  color: #fff;
+}
+</style>
---
+++ src/store.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+
+  },
+  // 同步
+  mutations: {
+
+  },
+  // 异步
+  actions: {
+
+  }
+})
